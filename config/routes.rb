786a712276ea0d472af8fdb4964b0b67bc12ca16
ShoyaSuzuki0523 <bellwood0523@gmail.com>
2019-04-16 08:31:38 +0000

    create product

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   devise_for :companies
   devise_for :colleges
   get 'home/top'
+  
+  get 'products/index'
+  get 'products/new'
+  post 'products/create'
+  post 'products/all_destroy'
+  get 'products/:id' => 'products#show'
+  get 'products/:id/edit' => 'products#edit'
+  post 'products/:id/update' => 'products#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
