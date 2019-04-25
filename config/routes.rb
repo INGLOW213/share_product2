d0b364c493b59e8ab5cb4373d04068820cb3cd30
EC2 Default User <ec2-user@ip-172-31-36-142.ec2.internal>
2019-04-25 08:24:38 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :companies
   devise_for :colleges
-  get 'home/top'
+  root to: 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
