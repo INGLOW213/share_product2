c468d2afbe312f6f13c13a8c2cd702c3db41d7cb
EC2 Default User <ec2-user@ip-172-31-36-142.ec2.internal>
2019-05-13 10:15:02 +0000

    product nest college

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_13_091351) do
-
 
   create_table "colleges", force: :cascade do |t|
     t.string "email", default: "", null: false
