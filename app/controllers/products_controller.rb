6b6131760ced029e1ce5edf5a478d77b8ade26a5
EC2 Default User <ec2-user@ip-172-31-84-7.ec2.internal>
2019-05-15 05:31:56 +0000

    add product edit function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,16 +31,16 @@ class ProductsController < ApplicationController
   
   def update
     @product = Product.find_by(id: params[:id])
-    @product.title = params[:title]
-    @product.content = params[:content]
-    @product.price = params[:price].to_i
-    @product.save
-    if @product.save
+    # @product.title = params[:title]
+    # @product.content = params[:content]
+    # @product.price = params[:price].to_i
+    # @product.save
+    if @product.update(product_params)
       flash[:success] = "保存しました"
-      redirect_to("/products/#{@product.id}")
+      redirect_to products_path
     else
       flash.now[:danger] = "保存できませんでした"
-      render("/product/#{@product.id}/edit")
+      render :edit
     end
   end
   
@@ -52,17 +52,17 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     @product.destroy
     flash[:warning] = "削除しました"
-    redirect_to("/products/index")
+    redirect_to products_path
   end
 
-  def all_destroy
-    @products = Product.all
-    @products.each do |product|
-      product.destroy
-    end
-    flash[:warning] = "すべて削除しました"
-    redirect_to("/products/index")
-  end
+  # def all_destroy
+  #   @products = Product.all
+  #   @products.each do |product|
+  #     product.destroy
+  #   end
+  #   flash[:warning] = "すべて削除しました"
+  #   redirect_to("/products/index")
+  # end
   
   private
     
